db1929b399fdee4f9850fea0c61bfe36f9e6a00c
MaxEichinger43 <mx.eichinger@gmx.de>
2023-12-12 17:47:58 +0100

    empty world, player

--- Main.py
+++ Main.py
@@ -14,57 +14,77 @@ gravity = 200
 font = pygame.font.Font(None, 24)
 
 class Player:
-    def __init__(self, x, y, gfx):
+    def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.gfx = gfx
-        self.mask = pygame.mask.from_threshold(self.gfx, (0,0,0,255), )
-        self.w = self.mask.get_size()[0]
-        self.h = self.mask.get_size()[1]
+        self.gfx = pygame.image.load("sprites/p1.png")
+        self.w = self.gfx.get_width()
+        self.h = self.gfx.get_height()
         self.speed = 500
-        self.body = self.mask.get_rect()
+        self.body = pygame.Rect(self.x, self.y, self.w, self.h)
         self.is_jumping = False
         self.is_falling = True
+
+
+    #def collisionDetection():
 
     def move(self):
         self.body.x = self.x
         self.body.y = self.y
 
+
     def grav(self):
-        self.y += gravity * (t_g ** 2) * dt
+        if self.is_falling:
+            self.y += gravity * dt
+        else:
+            pass
 
     def draw(self):
         screen.blit(self.gfx, (self.x, self.y))
-        pygame.draw.rect(screen, (255, 0, 0), self.body, 1, 1, 1)
 
-def input():
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_w]:
-        p1.y -= p1.speed * dt
-    if keys[pygame.K_s]:
-        p1.y += p1.speed * dt
-    if keys[pygame.K_a]:
-        p1.x -= p1.speed * dt
-    if keys[pygame.K_d]:
-        p1.x += p1.speed * dt
+    def input(self):
+        global debug_mode
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_w]:
+            self.y -= self.speed * dt
+        if keys[pygame.K_a]:
+            self.x -= self.speed * dt
+        if keys[pygame.K_s]:
+            self.y += self.speed * dt
+        if keys[pygame.K_d]:
+            self.x += self.speed * dt
 
-p1 = Player(random.randrange(0, scr_w), 0, pygame.image.load("sprites/p1.png"))
+        if keys[pygame.K_SPACE]:
+            p1.is_jumping = True
 
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-    clock.get_time()
+        if keys[pygame.K_F1]:
+            debug_mode = not debug_mode
 
-    screen.blit(background, (0, 0))
-    input()
-    p1.move()
-    p1.draw()
 
-    dt = clock.tick(60) / 1000
+def debug():
+    global p1_in_ground
     fps = f"Fps: {clock.get_fps():.0f}"
     debug_surface = font.render(fps, True, (255, 0, 0))
     screen.blit(debug_surface, (10, 10))
+
+    
+p1 = Player(random.randrange(0, scr_w), 0)
+
+debug_mode = True
+while running:
+    for event in pygame.event.get():
+        if (event.type == pygame.QUIT) or ((event.type == pygame.KEYDOWN) and (event.key == pygame.K_ESCAPE)):
+            running = False
+    clock.get_time()
+    screen.fill((130,255,255))
+    p1.input()
+    p1.move()
+    p1.draw()
+
+    if debug_mode:
+        debug()
+
     pygame.display.update()
+    dt = clock.tick(60) / 1000
 
 pygame.quit()
