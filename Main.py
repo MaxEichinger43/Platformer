736156597be187ee72af3ac58e1b8d9f04e69166
MaxEichinger43 <mx.eichinger@gmx.de>
2024-01-03 17:50:50 +0100

    updated debug funktion

--- Main.py
+++ Main.py
@@ -1,5 +1,4 @@
 import pygame
-import random
 pygame.init()
 
 scr_w = 1680
@@ -17,10 +16,8 @@ font = pygame.font.Font(None, 24)
 p_image = pygame.image.load("sprites/p1.png")
 p_image_f = p_image
 p_size = (p_image.get_width(), p_image.get_height())
-p_pos = pygame.Vector2(500,400)
-p_velo = pygame.Vector2(400,0)
-p_acc = 600
-p_decc = 800
+p_pos = pygame.Vector2(500, 400)
+p_velo = pygame.Vector2(400, 0)
 p_rect = pygame.Rect(p_pos, p_size)
 p_speed = p_velo.length()
 p_speed_max = 500
@@ -36,16 +33,18 @@ def movePlayer():
         p_image_f = pygame.transform.flip(p_image, 1, 0)
     else:
         p_image_f = p_image
-    
+
+
 def drawPlayer(p_image_f):
     screen.blit(p_image_f, p_pos)
+
 
 def controlPlayer(keys):
     global debug_mode, p_facingL, p_facingR
 
     if keys[pygame.K_w]:
         pass
-    
+
     if keys[pygame.K_a]:
         p_facingL = True
         p_facingR = False
@@ -55,7 +54,7 @@ def controlPlayer(keys):
 
     if keys[pygame.K_s]:
         pass
- 
+
     if keys[pygame.K_d]:
         p_facingL = False
         p_facingR = True
@@ -64,25 +63,31 @@ def controlPlayer(keys):
 
     if keys[pygame.K_SPACE]:
         pass
- 
-        if keys[pygame.K_F1]:
-            debug_mode = not debug_mode
+
+    if keys[pygame.K_F1]:
+        debug_mode = not debug_mode
 
 
 def debug():
-    global p1_in_ground
+    global p1_in_ground, p_pos, p_rect, p_speed, p_image_f, p_speed_max, p_facingR, p_facingL, dt
     fps = f"Fps: {clock.get_fps():.0f}"
     debug_surface = font.render(fps, True, (255, 0, 0))
     screen.blit(debug_surface, (10, 10))
     loc = f"Loc: {p_pos.x:.0f}, {p_pos.y:.0f}"
     debug_surface = font.render(loc, True, (255, 0, 0))
     screen.blit(debug_surface, (10, 30))
-    velo = f"velo: {p_velo.x:.0f}, {p_velo.y:.0f}"
+    velo = f"Velo: {p_velo.x:.0f}, {p_velo.y:.0f}"
     debug_surface = font.render(velo, True, (255, 0, 0))
     screen.blit(debug_surface, (10, 50))
-    acc = f"acc: {p_acc:.0f}"
-    debug_surface = font.render(acc, True, (255, 0, 0))
+    speed = f"Speed: {p_speed:.0f}"
+    debug_surface = font.render(speed, True, (255, 0, 0))
     screen.blit(debug_surface, (10, 70))
+    facing = f"Facing: {'Right' if p_facingR else 'Left'}"
+    debug_surface = font.render(facing, True, (255, 0, 0))
+    screen.blit(debug_surface, (10, 90))
+    dt_display = f"dt: {dt:.4f}"
+    debug_surface = font.render(dt_display, True, (255, 0, 0))
+    screen.blit(debug_surface, (10, 110))
 
 
 debug_mode = True
@@ -90,16 +95,15 @@ while running:
     for event in pygame.event.get():
         if (event.type == pygame.QUIT) or ((event.type == pygame.KEYDOWN) and (event.key == pygame.K_ESCAPE)):
             running = False
-    screen.fill((130,255,255))
+    screen.fill((130, 255, 255))
     clock.get_time()
     keys = pygame.key.get_pressed()
 
     controlPlayer(keys)
 
     movePlayer()
-    
-    drawPlayer(p_image_f)
 
+    drawPlayer(p_image_f)
 
     if debug_mode:
         debug()
