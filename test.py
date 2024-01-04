629e5ef2a5f517e7e62cf28b202d720b815018d8
MaxEichinger43 <mx.eichinger@gmx.de>
2024-01-04 11:16:39 +0100

    test updated

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
+import platform
+import random
 import pygame
 
 '''
@@ -27,11 +29,26 @@ jumping = False
 jt = 0
 ft = 0
 jmax = 2000
+jmin = 500
 jump_force = jmax
+jump_height = jmin
 
 '''
 Objects
 '''
+class Platform:
+    def __init__(self, pos, size):
+        self.pos = pos
+        self.size = size
+    def spawn(self):
+        self.rect = pygame.Rect(self.pos, self.size)
+    def update(self):
+        self.rect.x = self.pos[0]
+        self.rect.y = self.pos[1]
+        pygame.draw.rect(screen, (20, 200, 100), self.rect)
+        self.pos[0] -= 100 * dt
+        
+
 def check():
     global x, y, falling, imgl, img, imgfinal, grounded, ft, jt
     if y >= ground:
@@ -65,6 +82,8 @@ def update():
     elif jumping:
         jump()
     screen.blit(imgfinal, (x, y))
+    if y >= ground:
+        y = ground
 
 def startJump():
     global grounded, jumping, falling
@@ -73,18 +92,27 @@ def startJump():
     falling = False
 
 def jump():
-    global y, ft, jt, falling, jumping, jump_force
-    y -= jump_force * dt
-    jump_force -= gravity * dt
-    if jump_force <= 0:
+    global y, ft, jt, falling, jumping, jump_force, keys, jump_height
+    if jump_force <= 0 or jump_height >= jmax:
         endJump()
+    else:
+        y -= jump_force * dt
+        jump_force -= gravity * dt
+    if jump_height <= jmax and keys[pygame.K_SPACE]:
+        jump_height += jmax * dt
 
 def endJump():
-    global jumping, falling, jump_force
+    global jumping, falling, jump_force, jump_height
     jumping = False
     falling = True
     jump_force = jmax
+    jump_height = jmin
 
+
+platform1 = Platform([1700, ground - random.random() * 300], (800, 100))
+platform1.spawn()
+platform2 = Platform([1700, ground - random.random() * 300], (800, 100))
+platform2.spawn()
 
 '''
 Main loop
@@ -96,6 +124,8 @@ while running:
     screen.fill((130, 255, 255))
     clock.get_time()
     keys = pygame.key.get_pressed()
+    platform1.update()
+    platform2.update()
 
     check()
     handleInput(keys)
